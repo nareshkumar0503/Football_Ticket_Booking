daa7aedbdb40d804efde5370988149eda35de78d
NARESH KUMAR P <127754159+nareshkumar0503@users.noreply.github.com>
2026-02-13 08:44:43 +0530

    Connected PostgreSQL

--- sample/settings.py
+++ sample/settings.py
@@ -82,11 +82,19 @@ LOGOUT_REDIRECT_URL = 'login'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
